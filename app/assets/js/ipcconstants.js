70d0d30daa6592517a5bc37f81ca5b22b2647b44
DamsDev <loupegame@gmail.com>
2022-05-17 20:26:28 +0200

    Change ID of Azure Connection

--- app/assets/js/ipcconstants.js
+++ app/assets/js/ipcconstants.js
@@ -1,7 +1,7 @@
 // NOTE FOR THIRD-PARTY
 // REPLACE THIS CLIENT ID WITH YOUR APPLICATION ID.
 // SEE https://github.com/dscalzi/HeliosLauncher/blob/master/docs/MicrosoftAuth.md
-exports.AZURE_CLIENT_ID = '1ce6e35a-126f-48fd-97fb-54d143ac6d45'
+exports.AZURE_CLIENT_ID = '87cda755-ce12-44e2-98ff-ef8af908796b'
 // SEE NOTE ABOVE.
 
 
