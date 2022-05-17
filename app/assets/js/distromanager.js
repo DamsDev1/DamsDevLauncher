27dfa1b1b166317706399ba345515787b7377139
DamsDev <loupegame@gmail.com>
2022-05-17 18:40:39 +0200

    Update Distribution URL

--- app/assets/js/distromanager.js
+++ app/assets/js/distromanager.js
@@ -537,7 +537,7 @@ exports.pullRemote = function(){
         return exports.pullLocal()
     }
     return new Promise((resolve, reject) => {
-        const distroURL = 'https://magical-squirrel-cca9cd.netlify.app/distribution.json'
+        const distroURL = 'https://assets.stripfleax.tk/distribution.json'
         //const distroURL = 'https://gist.githubusercontent.com/dscalzi/53b1ba7a11d26a5c353f9d5ae484b71b/raw/'
         const opts = {
             url: distroURL,
